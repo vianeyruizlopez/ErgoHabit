e9e6fa71322005f501d5f53c873e190fdc3e1c43
Vianey Ruiz López <vianeyruizlopezsoftware@gmail.com>
2026-06-21 13:44:33 -0600

    Tareas-Cronometro1

--- src/main/kotlin/modules/usuarioFinal/frasesMotivacionales/infrastructure/persistence/FrasesMotivacionalesTable.kt
+++ src/main/kotlin/modules/usuarioFinal/frasesMotivacionales/infrastructure/persistence/FrasesMotivacionalesTable.kt
@@ -4,7 +4,7 @@ import org.jetbrains.exposed.sql.Table
 
 object FrasesMotivacionalesTable : Table("frases_motivacionales") {
     val idFrase = integer("id_frase").autoIncrement()
-    val categoria = varchar("categoria", 30) // 'WAKEUP', 'SLEEP', 'POSTURE', 'FOCUS', 'TASK_COMPLETE'
+    val categoria = varchar("categoria", 30)
     val texto = varchar("texto", 300)
 
     override val primaryKey = PrimaryKey(idFrase)
